2f37c554941e2810117b7dca3398818fcdf19b83
BrunoSouza <desouza88@hotmail.com>
2023-02-14 15:28:40 -0300

    Readme alterado

---
+++ src/pages/Fireworks.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import congrats from '../congrats.mp3';
+
+export default class Fireworks extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif" alt="fireworkgif" />
+        <img src="https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif" alt="fireworkgif" />
+        <img src="https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif" alt="fireworkgif" />
+        <audio src={ congrats } autoPlay><track kind="captions" /></audio>
+      </div>
+    );
+  }
+}
